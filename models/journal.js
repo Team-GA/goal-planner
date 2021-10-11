55f89ebc8c6bb999f47bdbd007c9f027f15675ec
hermanb <bideherman@yahoo.com>
2021-10-11 15:10:20 -0700

    updated models

--- models/journal.js
+++ models/journal.js
@@ -4,7 +4,11 @@ const journalSchema = new Schema(
     {
         journalDate: {type: Date, required: true},
         journalInput: {type: String, required: true},
-        userID: {type: String, required: true}
+        userID: {
+            type: Schema.Types.ObjectId, 
+            ref: "user",
+            required: true
+        }
     },
     {timestamps: true}
     );
