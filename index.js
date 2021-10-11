19850b00eb7ad172f7d8b0b72eefb4e69e60fa31
hermanb <bideherman@yahoo.com>
2021-10-11 14:44:26 -0700

    connected db to MongoDB database and to express

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const logger = require("morgan");
+// const api = require("./routes/index")
+require("./db");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(logger('dev'));
+
+// app.use("/api", api);
+
+app.get("/", (req, res) => {
+  res.status(200).json({message: "Express root route working!"})
+})
+
+app.listen(PORT, () => console.log(`You are listening to the port ${PORT}`))
