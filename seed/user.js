3af592b0eef13d3993460e52372800fb029f2feb
motah-fard <motah.fard@gmail.com>
2021-10-12 10:07:00 -0700

    all

---
+++ seed/user.js
@@ -0,0 +1,6 @@
+const db = require ("../db/index")
+const User = require("../models/user")
+const { hashPassword } = require('../utils/index')
+const createUsers = async () => {
+    const newUser
+}
