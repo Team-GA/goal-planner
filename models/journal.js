479f7cac5230db16ea1f5b7cff48ee2f4e1ceeeb
hermanb <bideherman@yahoo.com>
2021-10-11 20:48:38 -0700

    added route paths inside controllers, updated models and seed

--- models/journal.js
+++ models/journal.js
@@ -2,14 +2,14 @@ const { model, Schema } = require("mongoose");
 
 const journalSchema = new Schema(
     {
-        journalDate: {type: Date, required: true},
+        journalDate: { type: Date, default: Date.now, required: true},
         journalInput: {type: String, required: true},
         userID: {
             type: Schema.Types.ObjectId, 
             ref: "user",
             required: true
         },
-        date: { type: Date, default: Date.now}
+        
     },
     {timestamps: true}
     );
--- seed/journals.js
+++ seed/journals.js
@@ -1,27 +1,27 @@
-// const db = require('../db');
-// const Journal = require('../models/Journal')
+const db = require('../db');
+const Journal = require('../models/Journal')
 
 
-// const main = async () => {
-//     const [ journalUser ] = await User.find({ username: "Captn crunch"})
-//     const journal = [ 
-//         {
-//             journalDate: "Code or die",
-//             journalInput: "many people are considering a career change and are persuing coding",
-//             userId: journalUser._id
-//         }
+const main = async () => {
+    const [ journalUser ] = await User.find({ username: ""})
+    const journal = [ 
+        {
+            journalDate: 10/10/2021,
+            journalInput: "This is my journal, my truth and im sticking with it. I feel good writting in my journal and everyday i learn from it.",
+            userId: journalUser._id
+        }
   
-//     ]
+    ]
 
 
-//     await Post.insertMany(posts) 
-//         console.log('post has been created')
+    await Journal.insertMany(journal) 
+        console.log('Journal has been created')
     
-// }
+}
 
-// const run = async () => {
-//     await main();
-//     db.close();
-// }
+const run = async () => {
+    await main();
+    db.close();
+}
 
-// run();
+run();
