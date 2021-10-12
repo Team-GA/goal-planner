36a11f58f2460b0f3633a869537b560cc03a299a
hermanb <bideherman@yahoo.com>
2021-10-12 11:50:42 -0700

    updated seed/journals and routes/index

--- db/index.js
+++ db/index.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
 
 const MONGODB_URI =
-  process.env.PROD_MONGODB || "mongodb://127.0.0.1:27017/goalPlannerDatabase";
+  process.env.PROD_MONGODB || process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/goalPlannerDatabase";
 
 mongoose
   .connect(MONGODB_URI)
-  .then(() => console.log("Successfully connected to mongoDB"))
-  .catch((err) => console.log("MongoDB COnnection Error: ", err));
+  .then(() => console.log("Successfully connected to mongoDB."))
+  .catch((e) => console.error("MongoDB Connection Error: ", e.message));
 
 const db = mongoose.connection;
 
