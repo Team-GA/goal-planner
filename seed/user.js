810430e084a1d6284b617588ce516a68db0dfc04
motah-fard <motah.fard@gmail.com>
2021-10-12 10:27:11 -0700

    modified seed again

--- seed/user.js
+++ seed/user.js
@@ -1,9 +1,7 @@
 const db = require ("../db/index")
 const User = require("../models/user")
 const { hashPassword } = require('../utils/index')
-const createUsers = async () => {
-    const newUser
-}
+
 const createUsers = async () => {
     const users = [
         {
