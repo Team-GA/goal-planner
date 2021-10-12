995a0bbf3825db30d63e5c3b968f61274cc92e81
lynne <user@email.com>
2021-10-12 14:27:57 -0700

    fix toDo controller and routes

--- routes/toDo.js
+++ routes/toDo.js
@@ -7,8 +7,8 @@ const taskRouter = new Router();
 
 taskRouter.get("/task", restrict, taskController.getAllTasks );
 taskRouter.get("/task/:id", restrict, taskController.findTaskById );
-taskRouter.post("/newTask", restrict, taskController.createTask );
-taskRouter.put("/task/:id", restrict, taskController.updateTask );
-taskRouter.delete("/task/:id", restrict, taskController.deleteJournal );
+taskRouter.post("/newTask", restrict, taskController.createTasks );
+taskRouter.put("/task/:id", restrict, taskController.updateTasks );
+taskRouter.delete("/task/:id", restrict, taskController.deleteTask);
 
 module.exports = taskRouter;
