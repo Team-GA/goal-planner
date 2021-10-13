5b02d8a061808a3944b14803295519778c17a5cf
Kiana Abachi <Kiana@Kianas-Macbook-Air.local>
2021-10-13 11:56:00 -0700

    add navbar to app.js

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
-
-function Nav() {
+import { Link } from 'react-router-dom';
+export default function Nav() {
     return (
-        <div>
-            
-        </div>
+        <nav>
+            <Link to='/home' className="navItem">Home </Link>
+            <Link to='/add-to-do' className="navItem">Add to To-Do List </Link>
+            <Link to='/add-to-planner' className="navItem">Add to Planner </Link>
+            <Link to='/new-journal' className = 'navItem'>New Journal Entry</Link>
+            <Link to='/view-journal-entries' className = 'navItem'>View Journal Entries</Link>
+        </nav>
     )
 }
-
-export default Nav
