1f618f797d3ebca1038e10ca234c3f4064afe907
Kiana Abachi <Kiana@Kianas-Macbook-Air.local>
2021-10-14 12:44:24 -0700

    add restrict variable

--- routes/plannerEntry.js
+++ routes/plannerEntry.js
@@ -3,6 +3,8 @@ const plannerController = require("../controllers/plannerEntry");
 
 const plannerRouter = new Router();
 
+const { restrict } = require("../utils");
+
 plannerRouter.get("/planner", (req, res) => res.send("This is the root route!"));
 plannerRouter.get("/planner-entries", restrict, plannerController.getAllPlanners);
 plannerRouter.get("/planner-entries/:id",restrict, plannerController.getPlannerById);
