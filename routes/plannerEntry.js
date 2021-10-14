e55c2f847b351f67b7e674695260cc6035638c72
Kiana Abachi <Kiana@Kianas-Macbook-Air.local>
2021-10-14 12:43:07 -0700

    add restrict middleware to journal route and planner routes

--- routes/plannerEntry.js
+++ routes/plannerEntry.js
@@ -4,10 +4,10 @@ const plannerController = require("../controllers/plannerEntry");
 const plannerRouter = new Router();
 
 plannerRouter.get("/planner", (req, res) => res.send("This is the root route!"));
-plannerRouter.get("/planner-entries", plannerController.getAllPlanners);
-plannerRouter.get("/planner-entries/:id", plannerController.getPlannerById);
-plannerRouter.post("/planner-entries", plannerController.createPlanner);
-plannerRouter.put("/planner-entries/:id", plannerController.updatePlanner);
-plannerRouter.delete("/planner-entries/:id", plannerController.deletePlanner);
+plannerRouter.get("/planner-entries", restrict, plannerController.getAllPlanners);
+plannerRouter.get("/planner-entries/:id",restrict, plannerController.getPlannerById);
+plannerRouter.post("/planner-entries", restrict,  plannerController.createPlanner);
+plannerRouter.put("/planner-entries/:id", restrict,  plannerController.updatePlanner);
+plannerRouter.delete("/planner-entries/:id", restrict, plannerController.deletePlanner);
 
 module.exports = plannerRouter;
