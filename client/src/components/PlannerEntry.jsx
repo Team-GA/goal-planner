162d2c609f1d3cea0def3594717897fd5c4c2b74
hermanb <bideherman@yahoo.com>
2021-10-13 23:15:11 -0700

    updated ToDo

--- client/src/components/PlannerEntry.jsx
+++ client/src/components/PlannerEntry.jsx
@@ -4,6 +4,7 @@ const PlannerEntry = (props) => {
         <article>
             <h4>{ plannerDate }</h4>
             <h6>{ plannerInput }</h6>
+            <h3>Thus is the PlannerEntry</h3>
         </article>
     )
 }
