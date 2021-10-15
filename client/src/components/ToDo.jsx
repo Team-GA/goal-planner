8b9db02f08f655c4f73f4967080822fa763d8b5c
motah-fard <motah.fard@gmail.com>
2021-10-14 21:21:55 -0700

    delete button

--- client/src/components/ToDo.jsx
+++ client/src/components/ToDo.jsx
@@ -1,14 +1,22 @@
 import {Link} from "react-router-dom"
+import { deleteTask } from "../services/toDoServices";
+import { useHistory } from "react-router-dom";
 
 const ToDo = (props) => {
-    const { task } = props;
+    const  task  = props.task;
+    const ID = props.task._id;
+    const history = useHistory();
     // console.log(task)
-
+    const handelDelete = async () => {
+        await deleteTask(ID)
+        // history.push("/all-to-do")
+    }
     return(
         <article>
             <h4>{task.task}</h4>
             <Link to="/add-to-do">
                 <button>create a to do</button>
+                <button onClick={handelDelete}>X</button>
             </Link>
         </article>
     )
