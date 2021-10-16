cb93bc7eec8032ad77bc8dd7af9746597f8d8edb
motah-fard <motah.fard@gmail.com>
2021-10-16 14:21:29 -0700

    edit for todo and some changes

--- client/src/components/ToDoPage.jsx
+++ client/src/components/ToDoPage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {useEffect, useState} from "react";
 import ToDo from './ToDo';
 import { getAllTasks } from "../services/toDoServices"
+import {Link} from "react-router-dom"
 
 function ToDoPage() {
     const [tasks, setTasks] = useState([]);
@@ -16,6 +17,10 @@ function ToDoPage() {
     return (
         <div>
             <h4>To do list</h4>
+            <Link to="/add-to-do">
+                <button>create a to do</button>
+                
+            </Link>
             <div>
                 {tasks.map((toDoTask)=>(
                     <ToDo setToggleFetch={setToggleFetch} key={toDoTask._id} task={toDoTask}/>
